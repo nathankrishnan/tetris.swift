d52decbf37efe3637ad9878152b62fbd7bae300d
Nathan <nathan.a.krishnan@gmail.com>
2014-08-10 12:12:41 -0400

    Added clock timer functionality

--- Tetris/Tetris/GameScene.swift
+++ Tetris/Tetris/GameScene.swift
@@ -8,16 +8,23 @@
 
 import SpriteKit
 
-class GameScene: SKScene {
+// Represents the slowest speed at which the tetris blocks will travel
+let TickLengthLevelOne = NSTimeInterval(600)
 
+class GameScene: SKScene {
+    
+    // Defining closure and variables
+    var tick:(() -> ())?
+    var tickLengthMillis = TickLengthLevelOne
+    var lastTick:NSDate?
+    
     required init(coder aDecoder: NSCoder!) {
-    fatalError("NSCoder not supported")
+        fatalError("NSCoder not supported")
     }
     
     override init(size: CGSize) {
         super.init(size: size)
         
-        // anchor game in top-left corner of the screen
         anchorPoint = CGPoint(x: 0, y: 1.0)
         
         let background = SKSpriteNode(imageNamed: "background")
@@ -25,8 +32,29 @@ class GameScene: SKScene {
         background.anchorPoint = CGPoint(x: 0, y: 1.0)
         addChild(background)
     }
-
+    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        
+        // If lastTick is missing, we are in a paused state
+        // However, if present invoke timeIntervalSinceNow
+        if lastTick == nil {
+            return
+        }
+        // Multiply result by -1000 to get positive millisecond value
+        var timePassed = lastTick!.timeIntervalSinceNow * -1000.0
+        if timePassed > tickLengthMillis {
+            lastTick = NSDate.date()
+            tick?()
+        }
+    }
+    
+    // Accessor methos to let external classes stop and start the ticking process
+    func startTicking() {
+        lastTick = NSDate.date()
+    }
+    
+    func stopTicking() {
+        lastTick = nil
     }
 }
